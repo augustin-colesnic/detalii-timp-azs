028a4a1250fc2d83147a754b644231dbcc3946e1
Mihaela Colesnic <mihaela.colesnic@gmail.com>
2026-03-22 12:56:57 +0200

    adauga analitics

--- index.php
+++ index.php
@@ -36,6 +36,15 @@ $shortVerse = (mb_strlen($safeVerse) > 150) ? mb_substr($safeVerse, 0, 147) . ".
 <html lang="ro">
 
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-5LDRNQXXG7"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-5LDRNQXXG7');
+  </script>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php echo $defaultTitle; ?></title>
